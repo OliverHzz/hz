da384d189759b5c6b11054fada036814e923d704
HanZhao <lancer_hz@163.com>
2018-12-10 20:26:07 +0800

    init

---
+++ pages/liqueur/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    fgf
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'liqueur',
+  head() {
+    return {
+      title: '烧酒话'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
