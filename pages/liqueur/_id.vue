e382ac03b7ea78f307590854b7d78298d7d97b85
HanZhao <lancer_hz@163.com>
2018-12-20 20:34:51 +0800

    多页面完成

--- pages/liqueur/_id.vue
+++ pages/liqueur/_id.vue
@@ -5,50 +5,22 @@
       loop="loop"
       autoplay="autoplay">
       <source 
-        src="/music/liqueurMP3.mp3" 
+        :src="music"
         type="audio/mpeg">
     </audio>
-    <div>
-      <span>食品名称&nbsp;:&nbsp;</span>
-      <span>烧酒话（酱酒）2019年限量版 == 茅台镇第二传奇</span>
-    </div>
-    <div>
-      <span>配&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;料&nbsp;:&nbsp;</span>
-      <span>高粱、小麦、水</span>
-    </div>
-    <div>
-      <span>酒&nbsp;&nbsp;精&nbsp;&nbsp;度&nbsp;:&nbsp;</span>
-      <span>53%vol 净含量: 500ml</span>
-    </div>
-    <div>
-      <span>产品执行标准&nbsp;:&nbsp;</span>
-      <span>GB/T26760-2011（优级）</span>
-    </div>
-    <div>
-      <span>生产许可证号&nbsp;:&nbsp;</span>
-      <span>SC1155 2038 200372</span>
-    </div>
-    <div>
-      <span>生产日期&nbsp;:&nbsp;</span>
-      <span>见包装箱</span>
-    </div>
-    <div>
-      <span>贮存条件&nbsp;:&nbsp;</span>
-      <span>在阴凉，通风，干燥处保存</span>
-    </div>
-    <div>
-      <span>生产厂家&nbsp;:&nbsp;</span>
-      <span>贵州无忧酒业（集团）有限公司</span>
-    </div>
-    <div>
-      <span>产&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;地&nbsp;:&nbsp;</span>
-      <span>贵州省仁怀市茅台镇</span>
-    </div>
-    <div>
-      <span>全国统一零售价&nbsp;:&nbsp;</span>
-      <span>3300元/瓶</span>
+    <div 
+      v-for="(info, index) in infos" 
+      :key="index">
+      <span>{{ info.title }}</span>
+      <span>{{ info.text }}</span>
     </div>
     <p style="margin-top: 60px; margin-left: 24px;">过量饮酒有害健康，孕妇儿童不宜饮酒</p>
+    <section class="images">
+      <img
+        v-for="(image, index) in images"
+        :key="index"
+        :src="image">
+    </section>
   </section>
 </template>
 
@@ -56,8 +28,168 @@
 export default {
   layout: 'liqueur',
   head() {
+    let title = ''
+    if (this.$route.params.id === '1') {
+      title = '烧酒话'
+    } else if (this.$route.params.id === '2') {
+      title = '烧酒话'
+    } else if (this.$route.params.id === '3') {
+      title = '吴氏宗亲酒'
+    }
     return {
-      title: '烧酒话'
+      title: title
+    }
+  },
+  data() {
+    return {
+      infos: [],
+      music: '',
+      images: []
+    }
+  },
+  created() {
+    if (this.$route.params.id === '1') {
+      this.infos = [
+        {
+          title: '食品名称 : ',
+          text: '烧酒话（酱酒）2019年限量版 == 茅台镇第二传奇'
+        },
+        { title: '配 料 : ', text: '高粱、小麦、水' },
+        {
+          title: '酒 精 度 : ',
+          text: '53%vol'
+        },
+        {
+          title: '净含量 : ',
+          text: '500ml'
+        },
+        {
+          title: '产品执行标准 : ',
+          text: 'GB/T26760-2011（优级）'
+        },
+        {
+          title: '生产许可证号 : ',
+          text: 'SC1155 2038 200372'
+        },
+        {
+          title: '生产日期 : ',
+          text: '见包装箱'
+        },
+        {
+          title: '贮存条件 : ',
+          text: '在阴凉，通风，干燥处保存'
+        },
+        {
+          title: '生产厂家 : ',
+          text: '贵州无忧酒业（集团）有限公司'
+        },
+        {
+          title: '产 地 : ',
+          text: '贵州省仁怀市茅台镇'
+        },
+        {
+          title: '全国统一零售价 : ',
+          text: '3300元/瓶'
+        }
+      ]
+      this.music = '/music/liqueurMP3.mp3'
+      this.images = [
+        '/images/image01.jpg',
+        '/images/image02.jpg',
+        '/images/image03.jpg',
+        '/images/image04.jpg',
+        '/images/image05.jpg',
+        '/images/image06.jpg'
+      ]
+    } else if (this.$route.params.id === '2') {
+      this.infos = [
+        {
+          title: '食品名称 : ',
+          text: '烧酒话（酱酒）'
+        },
+        { title: '配 料 : ', text: '高粱、小麦、水' },
+        {
+          title: '酒 精 度 : ',
+          text: '53%vol'
+        },
+        {
+          title: '净含量 : ',
+          text: '500ml'
+        },
+        {
+          title: '产品执行标准 : ',
+          text: 'GB/T26760-2011（优级）'
+        },
+        {
+          title: '生产许可证号 : ',
+          text: 'SC1155 2038 200372'
+        },
+        {
+          title: '生产日期 : ',
+          text: '见包装箱'
+        },
+        {
+          title: '贮存条件 : ',
+          text: '在阴凉，通风，干燥处保存'
+        },
+        {
+          title: '生产厂家 : ',
+          text: '贵州无忧酒业（集团）有限公司'
+        },
+        {
+          title: '产 地 : ',
+          text: '贵州省仁怀市茅台镇'
+        },
+        {
+          title: '全国统一零售价 : ',
+          text: '1680元/瓶'
+        }
+      ]
+      this.music = '/music/liqueurMP3.mp3'
+      this.images = [
+        '/images/image01.jpg',
+        '/images/image02.jpg',
+        '/images/image03.jpg',
+        '/images/image04.jpg',
+        '/images/image05.jpg',
+        '/images/image06.jpg'
+      ]
+    } else if (this.$route.params.id === '3') {
+      this.infos = [
+        {
+          title: '食品名称 : ',
+          text: '吴氏宗亲酒（酱香型）'
+        },
+        { title: '配 料 : ', text: '高粱、小麦、水' },
+        {
+          title: '酒 精 度 : ',
+          text: '53%vol'
+        },
+        {
+          title: '净含量 : ',
+          text: '500ml'
+        },
+        {
+          title: '生产厂家 : ',
+          text: '贵州茅台镇赛台酒业集团'
+        },
+        {
+          title: '产 地 : ',
+          text: '贵州省仁怀市茅台镇'
+        },
+        {
+          title: '全国统一零售价 : ',
+          text: '598元/瓶'
+        }
+      ]
+      this.music = '/music/aipincaihuiying.mp3'
+      this.images = [
+        '/images/image01.jpg',
+        '/images/image02.jpg',
+        '/images/image03.jpg',
+        '/images/image04.jpg',
+        '/images/image05.jpg'
+      ]
     }
   },
   mounted() {
@@ -83,5 +215,15 @@ section > div {
 }
 section > div > span:nth-child(1) {
   flex-shrink: 0;
+}
+section > div > span:nth-child(2) {
+  margin-left: 5px;
+}
+.images {
+  width: 100%;
+}
+.images img {
+  display: block;
+  width: 100%;
 }
 </style>
